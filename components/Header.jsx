ce9236f12331dc96392e674051a9668a9471efca
Luis <luish.vargas09@gmail.com>
2023-07-12 09:49:52 -0600

    Restore the number of productos added to favorites (bookmarks)

--- components/Header.jsx
+++ components/Header.jsx
@@ -80,9 +80,7 @@ const Header = () => {
                     {/* Icon heart start */}
                     <div className='w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative'>
                         <IoMdHeartEmpty className='text-[15px] md:text-[20px]' />
-                        <div className='h-[14px] md:h-[20px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px] '>
-                            7
-                        </div>
+                        <div className='h-[14px] md:h-[20px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px] '></div>
                     </div>
                     {/* Icon Heart end */}
 
