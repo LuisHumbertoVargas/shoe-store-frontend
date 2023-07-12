e144530aefe8a4b2227350ac0cc9b5945e47f8de
Luis <luish.vargas09@gmail.com>
2023-07-12 10:07:41 -0600

    Partial change about product counter favorites (favorites = 0)

--- components/Header.jsx
+++ components/Header.jsx
@@ -80,7 +80,9 @@ const Header = () => {
                     {/* Icon heart start */}
                     <div className='w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative'>
                         <IoMdHeartEmpty className='text-[15px] md:text-[20px]' />
-                        <div className='h-[14px] md:h-[20px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px] '></div>
+                        <div className='h-[14px] md:h-[20px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px] '>
+                            0
+                        </div>
                     </div>
                     {/* Icon Heart end */}
 
